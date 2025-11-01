176006e482cd14ebdfd52427f48aa0d73d09ea4c
Cu-chi <qnt.ethan@gmail.com>
2025-11-01 23:29:33 +0100

    chore: install.sh base

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+REPO="https://raw.githubusercontent.com/Cu-chi/dotfiles/master"
+
+echo -e "${BLUE}Installation des dotfiles...${NC}"
+
+echo -e "${GREEN}✓ Installation terminée !${NC}"
