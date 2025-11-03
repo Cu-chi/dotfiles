c1722d7d1620f249ddf4376fa6ee6ec3101313bb
Ethan Quentin <equentin@z4r4p4.42lyon.fr>
2025-11-03 13:18:17 +0100

    tweak: oh-my-zsh installation

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ NC='\033[0m'
 
 REPO="https://raw.githubusercontent.com/Cu-chi/dotfiles/master"
 
+echo -e "${BLUE}Installation de oh-my-zsh...${NC}"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 echo -e "${BLUE}Installation des dotfiles...${NC}"
 
 echo -e "${GREEN}✓ Installation terminée !${NC}"
